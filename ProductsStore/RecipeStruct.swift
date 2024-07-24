04897c53460092dd733904f48d56269d2bc3a3db
Abhang Mane <abhang.goldmedal@gmail.com>
2024-07-24 13:18:02 +0530

    Next Commit

---
+++ ProductsStore/RecipeStruct.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeStruct.swift
+//  ProductsStore
+//
+//  Created by Abhang on 19/07/24.
+//
+
+import Foundation
---
+++ ProductsStore/RecipeTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeTableCell.swift
+//  ProductsStore
+//
+//  Created by Abhang on 19/07/24.
+//
+
+import UIKit
+
+class RecipeTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
